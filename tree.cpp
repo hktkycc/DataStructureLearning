676140029f94aba1b7de01564ce51d259a01ea1e
hktkycc <30361468+hktkycc@users.noreply.github.com>
2020-09-22 16:31:30 +0800

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -1,21 +1,110 @@
 #include <iostream>
 using namespace std;
 
-#define MAX 100
-typedef struct PTNode
-{
-	int data;
-	int parent;
-}PTNode;
-
-typedef struct
-{
-	PTNode nodes[MAX];
-	int r, n;
-}PTree;
 
 typedef struct BiTNode//二叉树定义
 {
 	int data;
 	struct BiTNode* left, * right;
-}BitNode,*BiTree;
+}BitNode, * BiTree;
+
+
+BiTNode* init()
+{
+	BiTNode* t;
+	t = (BiTNode*)malloc(sizeof(BiTNode));
+	t->data = 0;
+	t->left = NULL;
+	t->right = NULL;
+	return t;
+}
+
+void add(BiTNode* t, int data, int dir)
+{
+	BitNode* p = t;
+	BiTNode* m = init();
+	m->data = data;
+	if (dir % 2 == 0)
+		p->left = m;
+	else
+		p->right = m;
+}
+
+/*void remove(BiTNode* t, int e)
+{
+	int queue[100];
+	int front = 0, rear = 0;
+	BiTNode* p = t;
+	queue[0] = p->data;
+	rear = 1;
+	while (rear!=front)
+	{
+		front++;
+
+	}
+}*/
+
+
+void Preorder(BiTNode* t)
+{
+	BiTNode* p = t;
+	cout << p->data << " ";
+	if (p->left != NULL)
+		Preorder(p->left);
+	if (p->right != NULL)
+		Preorder(p->right);
+}
+
+
+void Inorder(BiTNode* t)
+{
+	BiTNode* p = t;
+	if(p->left!=NULL)
+		Inorder(p->left);
+	cout << p->data << " ";
+	if(p->right!=NULL)
+		Inorder(p->right);
+}
+
+
+void Bacorder(BiTNode* t)
+{
+	BiTNode* p = t;
+	if (p->left != NULL)
+		Bacorder(p->left);
+	if (p->right != NULL)
+		Bacorder(p->right);
+	cout << p->data << " ";
+}
+
+
+void show(BiTNode* t)
+{
+	BiTNode* p = t;
+	while(p!=NULL)
+	{
+		cout << p->data << " ";
+		p = p->right;
+	}
+}
+
+int main()
+{
+	BiTNode* tree;
+	tree = (BiTNode*)malloc(sizeof(BiTNode));
+	tree->data = 0;
+	add(tree, 1, 0);
+	add(tree, 2, 1);
+	add(tree->left, 3, 2);
+	add(tree->left, 4, 3);
+	add(tree->right, 5, 4);
+	add(tree->right, 6, 5);
+	cout << "PreOrder:  ";
+	Preorder(tree);
+	cout << endl;
+	cout << "InOrder:   ";
+	Inorder(tree);
+	cout << endl;
+	cout << "BackOrder: ";
+	Bacorder(tree);
+}
