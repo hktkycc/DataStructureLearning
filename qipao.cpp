6a2b70bd817def7594a29116a13d3f48947f4ecf
hktkycc <30361468+hktkycc@users.noreply.github.com>
2020-10-06 18:59:04 +0800

    Update qipao.cpp

--- qipao.cpp
+++ qipao.cpp
@@ -5,9 +5,9 @@ using namespace std;
 #define MAX 80000
 clock_t start,timeend;
 
-void sswap(int& a, int& b)
+void sswap(int *a, int *b)
 {
-	int c;
+	int *c;
 	c = a;
 	a = b;
 	b = c;
@@ -17,38 +17,40 @@ int main()
 {
 	start = clock();
 	int a[MAX];
+	int* ip = a;
 	for (int i = 0; i < MAX; i++)
 	{
-		a[i] = rand();
+		ip[i] = rand();
 		//cout << a[i] << " ";
 	}
 	cout << endl;
 	cout << "#";
 
 
-
+	int n = 0;
 	for (int x = MAX; x >= 0; x--)
 	{
+		int m = MAX - x;
 		int flag1 = 0;
 		for (int y = 0; y < x - 1; y++)
 		{
-			if (a[y] > a[y + 1])
+			if (ip[y] > ip[y + 1])
 			{
-				sswap(a[y], a[y + 1]);
+				sswap(&ip[y], &ip[y + 1]);
 				flag1++;
 			}
 		}
 		if (flag1 == 0)
 			x = -1;
+		if (m % (MAX/100) == 0)
+		{
+			n++;
+			cout << n << "%";
+		}
 	}
 
 	timeend = clock();
 	double endtime = (double)(timeend - start);
-
-	for (int i = 0; i < MAX; i++)
-	{
-		cout << a[i] << " ";
-	}
 
 	cout << endl;
 	cout << "Total time:" << endtime << "ms";
