7ff63fdd1bb4a82600f7f236b78e2d6b82820412
hktkycc <30361468+hktkycc@users.noreply.github.com>
2020-10-08 16:14:47 +0800

    Create shell_sort.cpp

---
+++ shell_sort.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <cstdlib>
+#include<ctime>
+using namespace std;
+#define MAX 80000
+clock_t start, timeend, start1, timeend1;
+
+void shell_sort_switch(int a[], int length)
+{
+	int h = 1;
+	int n = 2;
+	while (h < length / n)
+	{
+		h = n * h + 1;
+	}
+	while (h >= 1) {
+		for (int i = h; i < length; i++)
+		{
+			for (int j = i; j >= h && a[j] < a[j - h]; j -= h)
+			{
+				swap(a[j], a[j - h]);
+			}
+		}
+		h = h / n;
+	}
+}//交换法希尔排序
+
+void shell_sort_insert(int a[], int length)
+{
+	int h = 1;
+	int n = 2;
+	int temp = 0;
+	while (h < length / n)
+	{
+		h = n * h + 1;
+	}
+	while (h >= 1) {
+		for (int i = h; i < length; i += h)
+		{
+			temp = a[i];
+			for (int j = i; j > 0; j -= h)
+			{
+				if (temp < a[j - h])
+				{
+					a[j] = a[j - h];
+				}
+				else
+				{
+					a[j] = temp;
+					break;
+				}
+			}
+		}
+		h = h / n;
+	}
+}//插入法希尔排序
+
+int main()
+{
+
+	int a[MAX],insert[MAX];
+	for (int i = 0; i < MAX; i++)
+	{
+		a[i] = rand();
+		insert[i] = a[i];
+		//cout << a[i] << " ";
+	}
+	cout << endl;
+	cout << "#";
+
+	start = clock();
+	shell_sort_switch(a, MAX);
+	timeend = clock();
+	double endtime = (double)(timeend - start);
+
+	start1 = clock();
+	shell_sort_insert(insert, MAX);
+	timeend1 = clock();
+	double endtime1 = (double)(timeend1 - start1);
+
+	cout << endl;
+	cout << "switch time:" << endtime << "ms";
+	cout << endl;
+	cout << "insert time:" << endtime1 << "ms";
+
+
+}
